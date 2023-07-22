12d17a29b53d97c16054018506c9a15094beb39f
bbangHo <sonsins1@naver.com>
2023-07-22 23:51:12 +0900

    서비스 추가 사항
    - 대화방 생성
    - 채팅방 입장시 안 읽은 메시지 리턴
    - 채팅 페이지 진입시 팀 개인, 팀 채팅 구분하여 반환하도록 변경
    - 메시지 저장 및 전송
    - 메시지를 읽은 시각 업데이트

--- SNACK/src/main/java/com/example/demo/Chat/ChatService.java
+++ SNACK/src/main/java/com/example/demo/Chat/ChatService.java
@@ -1,46 +1,42 @@
 package com.example.demo.Chat;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.example.demo.Chat.Dto.ChatRoomDTO;
+import com.example.demo.Chat.Dto.MessageDTO;
+import com.example.demo.Chat.Entity.ChatMessage;
+import com.example.demo.Chat.Entity.ChatRoom;
+import com.example.demo.Chat.Entity.ChatRoomMember;
 import com.example.demo.Chat.repository.ChatMessageRepository;
 import com.example.demo.Chat.repository.ChatRoomMemberRepository;
+import com.example.demo.Chat.repository.ChatRoomRepository;
 import com.example.demo.Member.MemberEntity;
 import com.example.demo.Member.MemberRepository;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
 @Service
 @AllArgsConstructor
+@Log4j2
 public class ChatService {
-	private final ComponentService componentService;
-	
-	/*
-	 * 사용자 정보를 받아서 사용자의 채팅 내역을 리턴해주는 메서드
-	 * 
-	 * @param 정해야함
-	 * 
-	 * @return Exception
-	 * 
-	 */
-	public List<ChatRoomDTO.Get> getChatList(Long member_id) {
-		return componentService.getChatList(member_id);
-	}
-}
-
-@Service
-@AllArgsConstructor
-class ComponentService {
-	private final MemberRepository memberRepository;
+	private final MemberRepository memberRepository; // 나중에 멤버 서비스의 메소드로 대체할 예정
 	private final ChatRoomMemberRepository chatRoomMemberRepository;
 	private final ChatMessageRepository chatMessageRepository;
-
+	private final ChatRoomRepository chatRoomRepository;
+	private final Mapper mapper;
+	
 	/*
 	 * 사용자가 속한 채팅방을 모두 조회하여 리턴한다.
 	 * 
@@ -49,35 +45,169 @@ class ComponentService {
 	 * @return 사용자가 속한 채팅방 리스트를 DTO로 반환 Exception 사용자가 검색되지 않을 경우 ChatException을
 	 * 발생시킴
 	 */
+	@Transactional(readOnly = true)
 	public List<ChatRoomDTO.Get> getChatList(Long member_id) {
+		log.info("--getChatList--");
+		
 		MemberEntity memberEntity = verifiedMember(member_id);
-		
-		List<ChatRoomMember> chatRoomMember = chatRoomMemberRepository.findMembersWithSameRoom(member_id);
+
+		List<Object[]> crmAndrt = chatRoomMemberRepository.findMembersWithSameRoomAndReceiveTime(member_id);
 		List<ChatRoomDTO.Get> chatList = new ArrayList<>();
+
+		Map<Long, ChatRoomDTO.Get> chatMap = new HashMap<>();
+
+	    for (Object[] list : crmAndrt) {
+	        ChatRoomMember crm = (ChatRoomMember) list[0];
+	        LocalDateTime ldt = (LocalDateTime) list[1];
+
+	        Long roomId = crm.getChatRoom().getRoomId();
+	        ChatRoomDTO.Get cr = chatMap.get(roomId);
+
+	        if (cr == null) {
+	            cr = new ChatRoomDTO.Get();
+	            boolean isTeam = crm.getChatRoom().getTeam() != null;
+	            
+	            cr.setType(isTeam ? "team" : "private");
+	            cr.setRoomId(roomId);
+	            cr.setName(isTeam ? crm.getChatRoom().getTeam().getTeamName() : crm.getMember().getNickname());
+	            cr.setNumberOfUnreadMessage(chatMessageRepository.getNumberOfUnreadMessage(member_id, roomId));
+	            cr.setTime(ldt);
+	            
+	            chatMap.put(roomId, cr);
+	        }
+	    }
+	    
+	    return chatMap.values().stream()
+	            .sorted(Comparator.comparing(ChatRoomDTO.Get::getTime).reversed())
+	            .collect(Collectors.toList());
+	}
+	
+	/*
+	 * 팀에 어떤 사용자가 추가되었을 때 채팅방에 해당 사용자를 추가하는 메서드
+	 */
+	
+	
+	
+	/*
+	 * 채팅방을 생성하는 메서드, A가 B에게 처음 채팅을 걸려고 할 때
+	 * 
+	 * @param userName B의 닉네임에 해당
+	 * @return chatRoom.getRoomId() 
+	 */
+	public Long createPrivateChatRoom(Long myMemberId, String theOtherMemberName) {
+		// 사용자 닉네임으로 찾는 부분
+//		MemberEntity member = memberRepository.getByNickName();
+		MemberEntity member = new MemberEntity(111L, theOtherMemberName);	// 임시
 		
-		for(ChatRoomMember crm : chatRoomMember) {
-			ChatRoomDTO.Get cr = new ChatRoomDTO.Get();
-			
-			cr.setRoom_id(crm.getChatRoom().getRoom_id());
-			cr.setMember_id(crm.getMember().getMember_id());
-			cr.setName(crm.getMember().getNickname());
-			// 성능이 떨어질 것 같은데, 어떻게 해야할지 모르겠음 DB에서 최대한 쿼리를 쳐서 가져와야 하는데, 
-			// 각 room 마다 쿼리를 하나 지금 이 방식이나 비슷할 것 같음(지금 생각으로는),, 나중에 리팩토링 해야할 것 같음
-			cr.setMessage_count(chatMessageRepository.getUnReadMessageCount(crm.getChatRoom().getRoom_id()));	
-			cr.setRoom_id(crm.getChatRoom().getRoom_id());
-			
-			chatList.add(cr);
+		ChatRoom chatRoom = chatRoomRepository.saveAndFlush(new ChatRoom());
+		
+		ChatRoomMember chatRoomMemeber = ChatRoomMember.builder()
+				.chatRoom(chatRoom)
+				.member(member)
+				.readTime(LocalDateTime.now())
+				.build();
+		
+		chatRoomMemberRepository.save(chatRoomMemeber);
+		
+		return chatRoom.getRoomId();
+	}
+	
+	/*
+	 * 사용자가 전송한 메시지를 db에 저장함
+	 * 
+	 * @param MessageDTO.Request mdto 메시지 틀
+	 * @return 없음 exception
+	 */
+	@Transactional
+	public void saveMessage(MessageDTO.Request mdto) {
+		log.info("--saveMessage--");
+		
+		ChatRoom ChatRoomEntity = verifiedChatRoom(mdto.getRoomId());
+		MemberEntity memberEntity = verifiedMember(mdto.getSenderId());
+
+		ChatMessage chatMessageEntity = ChatMessage.builder()
+				.chatRoom(ChatRoomEntity)
+				.sender(memberEntity)
+				.content(mdto.getContent())
+				.sentAt(LocalDateTime.now())
+				.build();
+
+		chatMessageRepository.save(chatMessageEntity);
+	}
+	
+	/*
+	 * 사용자가 채팅방에 들어갔을 때 안 읽은 메시지들을 보내줌 
+	 */
+	@Transactional(readOnly = true)
+	public List<MessageDTO.Response> getChatMessage(Long memberId, Long roomId) {
+		log.info("--getChatMessage--");
+		
+		List<ChatMessage> unreadChatMessages = chatMessageRepository.getUnreadMessages(memberId, roomId);
+		List<MessageDTO.Response> messageDTOResponse = new ArrayList<>();
+		
+		for(ChatMessage cm : unreadChatMessages) {
+			MessageDTO.Response mdtor = mapper.ChatMessageToMessageDtoResponse(cm);
+			messageDTOResponse.add(mdtor);
 		}
 		
-		// 질문할 것, 정렬의 기준은 가장 최근의 메시지 중 안 읽은 메시지인지?
-		return chatList.stream().sorted(Comparator.comparing(ChatRoomDTO.Get::getMessage_count).reversed()).collect(Collectors.toList());
+		return messageDTOResponse;
 	}
-
+	
 	/*
-	 * 사용자가 DB에 존재하는지 확인하는 메서드 
+	 * 채팅방 삭제
 	 */
-	public MemberEntity verifiedMember(Long member_id) {
-		Optional<MemberEntity> optionalMemberEntity = memberRepository.findById(member_id);
+	@Transactional
+	public void deleteChatRoom(Long memberId, Long roomId) {
+		log.info("--deleteChatRoom--");
+		chatMessageRepository.deleteByRoomId(roomId);
+		chatRoomMemberRepository.deleteByRoomId(roomId);
+		chatRoomRepository.deleteByRoomId(roomId);
+	}
+	
+	/*
+	 * 정기적으로 채팅방에 속한 모든 사용자가 읽은 채팅은 삭제
+	 */
+//	public void deleteChatMessage() {
+//		List<ChatRoom> chatRooms = chatRoomRepository.findAll();
+//		
+//		for(ChatRoom cr : chatRooms) {
+//			chatRoomMemberRepository.findOldestReadTimeByChatRoom(cr);
+//			
+//		}
+//	}
+	
+	// 사용자가 어떤 채팅방을 읽은 시간을 저장함
+	@Transactional
+	public void setReadingTime(Long memberId, Long roomId) {
+		log.info("--setReadingTime--");
+		
+		// crm not found 예외 처리 해야함
+		ChatRoomMember crm = chatRoomMemberRepository.findByMemberIdAndRoomId(memberId, roomId);
+		crm.setReadTime(LocalDateTime.now());
+		chatRoomMemberRepository.save(crm);
+	}
+	
+//	public MemberEntity findByMemberName(Long memberId) {
+//		log.info("--verifiedMember--");
+//		
+//		Optional<MemberEntity> optionalMemberEntity = memberRepository.findBy
+//	}
+	
+	@Cacheable(cacheNames = "members")	// 캐싱이 안되는데 원인을 모르겠음
+	public MemberEntity verifiedMember(Long memberId) {
+		log.info("--verifiedMember--");
+		
+		Optional<MemberEntity> optionalMemberEntity = memberRepository.findById(memberId);
 		return optionalMemberEntity.orElseThrow(() -> new ChatException(ErrorCode.MEMBER_NOT_FOUND));
 	}
+
+	@Cacheable(cacheNames = "chatRooms")	// 캐싱이 안되는데 원인을 모르겠음
+	public ChatRoom verifiedChatRoom(Long roomId) {
+		log.info("--verifiedChatRoom--");
+		
+		Optional<ChatRoom> optionalChatRoom = chatRoomRepository.findById(roomId);
+		return optionalChatRoom.orElseThrow(() -> new ChatException(ErrorCode.ROOM_NOT_FOUND));
+	}
 }
+
+
