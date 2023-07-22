cb8e8d8cc5923ca9e34c537621575281eb5ab971
bbangHo <sonsins1@naver.com>
2023-07-22 23:48:47 +0900

    [200~쿼리 추가 (시간되면 queryDSL 로 재작성 예정) > > - 사용자별 안 읽은 메시지 수 & 안 읽은 메시지 반환 쿼리 > - 각 채팅방 상황별 조회에 관한 쿼리 추가 > - 채팅방 나가기 팀 삭제 등으로 인한 삭제 쿼리 추가 >

--- SNACK/src/main/java/com/example/demo/Chat/repository/ChatMessageRepository.java
+++ SNACK/src/main/java/com/example/demo/Chat/repository/ChatMessageRepository.java
@@ -1,14 +1,48 @@
 package com.example.demo.Chat.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.example.demo.Chat.ChatMessage;
+import com.example.demo.Chat.Entity.ChatMessage;
 
 @Repository
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long>{
-	@Query("SELECT COUNT(*) FROM ChatMessage cm WHERE cm.chatRoom.id = :chatRoom_id AND cm.isRead = false")
-	int getUnReadMessageCount(@Param("chatRoom_id") Long chatRoom_id);
+	@Query("SELECT COUNT(*) "
+			+ "FROM ChatMessage cm "
+			+ "WHERE cm.chatRoom.id = :chatRoom_id "
+			+ "AND cm.sentAt < ("
+				+ "SELECT crm.readTime "
+				+ "FROM ChatRoomMember crm "
+				+ "WHERE crm.member.id = :my_member_id "
+				+ "ORDER BY crm.readTime DESC "
+				+ "LIMIT 1)")
+	int getNumberOfUnreadMessage(@Param("my_member_id") Long my_member_id, @Param("chatRoom_id") Long chatRoom_id);
+	
+//	@Query("SELECT cm "
+//			+ "FROM ChatMessage cm "
+//			+ "JOIN FETCH ChatRoomMember crm "
+//			+ "WHERE cm.chatRoom.id = crm.chatRoom.id "
+//				+ "AND cm.chatRoom.id = :chatRoom_id "
+//				+ "AND cm.sentAt < (SELECT crm2.sentAt FROM ChatRoomMember crm2 WHERE crm2.member.id = :memberID AND crm2.chatRoom.id = :chatRoom.id) "
+//				+ "AND cm.sender.id != :memberId")
+//	List<ChatMessage> getUnreadMessages(@Param("memberId") Long memberId, @Param("chatRoom_id") Long chatRoom_id);
+	
+	@Query("SELECT cm FROM ChatMessage cm "
+            + "JOIN FETCH ChatRoomMember crm "
+            + "ON crm.member.id = :memberId "
+            + "WHERE cm.chatRoom.id = :chatRoomId "
+            + "AND cm.sentAt >= (SELECT crm2.readTime FROM ChatRoomMember crm2 "
+            					+ "WHERE crm2.member.id = :memberId "
+            					+ "AND crm2.chatRoom.id = :chatRoomId) "
+            + "AND cm.sender.id != :memberId")
+    List<ChatMessage> getUnreadMessages(@Param("memberId")Long memberId, @Param("chatRoomId")Long chatRoomId);
+	
+	@Modifying
+	@Query("DELETE FROM ChatMessage cm WHERE cm.chatRoom.id = :roomId")
+	int deleteByRoomId(@Param("roomId") Long roomId);
 }
--- SNACK/src/main/java/com/example/demo/Chat/repository/ChatRoomRepository.java
+++ SNACK/src/main/java/com/example/demo/Chat/repository/ChatRoomRepository.java
@@ -1,11 +1,17 @@
 package com.example.demo.Chat.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.example.demo.Chat.ChatRoom;
+import com.example.demo.Chat.Entity.ChatRoom;
 
 @Repository
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long>{
 	
+	@Modifying
+	@Query("DELETE FROM ChatRoom cr WHERE cr.roomId = :roomId")
+	int deleteByRoomId(@Param("roomId") Long roomId);
 }
